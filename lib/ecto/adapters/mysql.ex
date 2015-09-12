7deb0ba5739899a3700c3a5d7137d6695056af59
Richard Bishop <richard@rubiquity.com>
2015-09-11 23:02:03 -0600

    Apply patch to Ecto 0.12 :(

--- lib/ecto/adapters/mysql.ex
+++ lib/ecto/adapters/mysql.ex
@@ -85,7 +85,7 @@ defmodule Ecto.Adapters.MySQL do
   """
 
   # Inherit all behaviour from Ecto.Adapters.SQL
-  use Ecto.Adapters.SQL, :mariaex
+  use Ecto.Adapters.SQL, :mysqlex
 
   # And provide a custom storage implementation
   @behaviour Ecto.Adapter.Storage
